346aea954968d7fd812d100388d410d58d112f21
BarG3126 <bar3126@gmail.com>
2025-01-18 13:15:53 +0200

    functionality for initializing DVC storage

--- bullyguard/version_data.py
+++ bullyguard/version_data.py
@@ -1,11 +1,13 @@
 from bullyguard.config_schemas.config_schema import Config
 from bullyguard.utils.config_utils import get_config
-from bullyguard.utils.data_utils import initialize_dvc
+from bullyguard.utils.data_utils import initialize_dvc, initialize_dvc_storage
 
 
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
     initialize_dvc()
+
+    initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
 
 
 if __name__ == "__main__":
