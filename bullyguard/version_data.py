4f9f64a6a360677ecb13a387b7719387ff115413
BarG3126 <bar3126@gmail.com>
2025-01-18 14:47:45 +0200

    add dvc commit functionality

--- bullyguard/version_data.py
+++ bullyguard/version_data.py
@@ -1,6 +1,6 @@
 from bullyguard.config_schemas.config_schema import Config
 from bullyguard.utils.config_utils import get_config
-from bullyguard.utils.data_utils import initialize_dvc, initialize_dvc_storage
+from bullyguard.utils.data_utils import initialize_dvc, initialize_dvc_storage, commit_to_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -8,6 +8,8 @@ def version_data(config: Config) -> None:
     initialize_dvc()
 
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
+
+    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 
 if __name__ == "__main__":
