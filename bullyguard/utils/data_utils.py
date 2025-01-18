87e13e9a92567587c1f26d47d243e4116ab06ae6
BarG3126 <bar3126@gmail.com>
2025-01-18 12:33:53 +0200

    fix some issues

--- bullyguard/utils/data_utils.py
+++ bullyguard/utils/data_utils.py
@@ -13,9 +13,10 @@ def initialize_dvc() -> None:
     if is_dvc_initialized():
         DATA_UTILS_LOGGER.info("DVC is already initialized")
         return
+
     DATA_UTILS_LOGGER.info("Initializing DVC")
     run_shell_command("dvc init")
     run_shell_command("dvc config core.analytics false")
     run_shell_command("dvc config core.autostage true")
     run_shell_command("git add .dvc")
-    run_shell_command("git commit -m 'initialized DVC'")
+    run_shell_command("git commit -nm 'Initialized DVC'")
