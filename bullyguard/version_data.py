b809dc17ff789072aa2a906e38f2486e265d29ea
BarG3126 <bar3126@gmail.com>
2025-01-30 10:44:52 +0200

    Updated version of the data from v1 to v2

--- bullyguard/version_data.py
+++ bullyguard/version_data.py
@@ -1,6 +1,6 @@
 from bullyguard.config_schemas.config_schema import Config
 from bullyguard.utils.config_utils import get_config
-from bullyguard.utils.data_utils import initialize_dvc, initialize_dvc_storage, commit_to_dvc
+from bullyguard.utils.data_utils import initialize_dvc, initialize_dvc_storage, make_new_data_version
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -9,7 +9,7 @@ def version_data(config: Config) -> None:
 
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
 
-    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
+    make_new_data_version(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 
 if __name__ == "__main__":
